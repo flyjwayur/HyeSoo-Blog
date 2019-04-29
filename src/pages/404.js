a4771fc8ded845c36189c76c5d82ecd3108ea66f
Hye Soo Park <flyjwayur@gmail.com>
2019-04-29 09:01:35 +0300

    Add global style to the pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,10 @@
 import React from "react"
+import styles from "./404.module.css"
 
 //https://github.com/gatsbyjs/gatsby/blob/master/docs/docs/add-404-page.md
 export default () => (
   <div>
-    <h1>Page not found</h1>
-    <p>The page is not found</p>
+    <h1 className={styles.header}>Page not found</h1>
+    <p className={styles.errorMessage}>The page is not found</p>
   </div>
 )
