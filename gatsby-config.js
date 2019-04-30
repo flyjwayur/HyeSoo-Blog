ef6db5986028b97b97518220fdd5c51e0989d5b2
Hye Soo Park <flyjwayur@gmail.com>
2019-04-30 17:20:41 +0300

    Fetch file source information with plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,14 @@ module.exports = {
   siteMetadata: {
     title: "HyeSoo's blog ",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "files",
+        path: `${__dirname}/src/pages`,
+      },
+    },
+  ],
 }
