831ab8bda3f98d63d9132f71125d1bce8aeef8c9
Hye Soo Park <flyjwayur@gmail.com>
2019-04-30 14:28:24 +0300

    Add header button and links with styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,20 +2,53 @@ import React from "react"
 import { Link } from "gatsby"
 import styles from "./header.module.scss"
 
+//HeaderLink component
+const HeaderLink = props => (
+  <Link className={styles.link} to={props.to}>
+    {props.text}
+  </Link>
+)
+
+// HomeButton component
+const HomeButton = props => (
+  <Link to={props.to}>
+    <div className={styles.homeButton}>{props.text}</div>
+  </Link>
+)
+
+const SocialButton = props => {
+  let style = styles.socialMediaButton
+  let url = ""
+
+  if (props.site === "linkedin") {
+    url = "https://www.linkedin.com/in/" + props.username
+  } else if (props.site === "github") {
+    url = "https://www.github.com/" + props.username
+  }
+
+  return (
+    //no referrer information to be leaked on following the link
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className={style}>{props.children}</div>
+    </a>
+  )
+}
+
 export default () => (
   <header className={styles.container}>
     <div className={styles.row}>
-      <Link to="/">HyeSoo's Blog</Link>
+      <HomeButton to="/" text="HyeSoo's blog" />
+      <SocialButton site="github" username="flyjwayur">
+        Github
+      </SocialButton>
+      <SocialButton site="linkedin" username="hyesoopark-developer">
+        Linked In
+      </SocialButton>
     </div>
 
     <div className={styles.row}>
-      <Link className={styles.link} to="/">
-        Home
-      </Link>
-      |
-      <Link className={styles.link} to="/about">
-        About
-      </Link>
+      <HeaderLink className={styles.link} to="/" text="Home" />|
+      <HeaderLink className={styles.link} to="/about" text="About" />
     </div>
   </header>
 )
